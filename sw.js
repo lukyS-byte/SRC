85072f578740efc259063dcfcd41885d1805d3b2
lukyS-byte <luki.bukova@gmail.com>
2026-03-09 13:31:26 +0100

    Fix SW: network first, clear all old caches

--- sw.js
+++ sw.js
@@ -1,22 +1,26 @@
-const CACHE = 'src-v1';
-const ASSETS = ['./', './index.html', './manifest.json'];
+const CACHE = 'src-v3';
+const ASSETS = ['./manifest.json'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener('fetch', e => {
+  // Always try network first, fallback to cache only when offline
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+    fetch(e.request).then(res => {
       const clone = res.clone();
       caches.open(CACHE).then(c => c.put(e.request, clone));
       return res;
-    }))
+    }).catch(() => caches.match(e.request))
   );
 });
